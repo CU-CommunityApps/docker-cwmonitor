fd7fbb4d32b9649cdcc4a081ab3288a0282db66a
sbower <shawn.bower@gmail.com>
2015-09-16 00:45:37 -0400

    small change to monitor

--- monitor.rb
+++ monitor.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
 while 1 do
-  puts `/aws-scripts-mon/mon-put-instance-data.pl --mem-util --swap-util --disk-path=/ --disk-space-util`
+  puts `/aws-scripts-mon/mon-put-instance-data.pl --mem-util --swap-util --disk-path=/rootfs --disk-space-util`
   sleep(300)
 end
