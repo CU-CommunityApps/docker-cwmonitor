945c509478f5b7020abcdebada9b782715f8d7c9
sbower <shawn.bower@gmail.com>
2016-04-12 19:35:22 -0400

    adding ability to specify multiple disk paths

--- monitor.rb
+++ monitor.rb
@@ -1,6 +1,14 @@
 #!/usr/bin/env ruby
 
+disk_path = "--disk-path=/rootfs"
+if ENV["PATHS"]
+  ENV["PATHS"].split(",").each do |path|
+    disk_path += " --disk-path=/rootfs" + path
+    puts disk_path
+  end
+end
+
 while 1 do
-  puts `/aws-scripts-mon/mon-put-instance-data.pl --mem-util --swap-util --disk-path=/rootfs --disk-space-util`
+  puts `/aws-scripts-mon/mon-put-instance-data.pl --mem-util --swap-util #{disk_path} --disk-space-util`
   sleep(300)
 end
