71c657e80a870fcd55fb2126aab2d10713f4ccf4
Ian Dillon <isd23@cornell.edu>
2018-02-02 10:55:48 -0500

    Adds optional ASG flag for ASG instance reporting.

--- monitor.rb
+++ monitor.rb
@@ -7,7 +7,12 @@ if ENV["PATHS"]
   end
 end
 
+asg_flag = ""
+if ENV["IS_ASG"]
+  asg_flag = "--auto-scaling"
+end
+
 while 1 do
-  puts `/aws-scripts-mon/mon-put-instance-data.pl --mem-util --swap-util #{disk_path} --disk-space-util`
+  puts `/aws-scripts-mon/mon-put-instance-data.pl --mem-util --swap-util #{disk_path} --disk-space-util #{asg_flag}`
   sleep(300)
 end
